32f1f4dca50d38725abbba09f8330c6645875246
leo_da_cruz <leonardosilveiracrux@gmail.com>
2024-10-15 01:40:21 -0300

    Abrigo DTO

---
+++ microServicos/src/main/java/prat_eng_soft_api_test_automatizado/DTO/AbrigoDTO.java
@@ -0,0 +1,49 @@
+package prat_eng_soft_api_test_automatizado.DTO;
+
+public class AbrigoDTO {
+    private String name;
+    private String phone;
+    private String email;
+    private AbrigoAddressDTO address;
+
+    public AbrigoDTO(String name, String phone, String email, AbrigoAddressDTO address) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public AbrigoAddressDTO getAddress() {
+        return address;
+    }
+
+    public void setAddress(AbrigoAddressDTO address) {
+        this.address = address;
+    }
+
+    
+}
