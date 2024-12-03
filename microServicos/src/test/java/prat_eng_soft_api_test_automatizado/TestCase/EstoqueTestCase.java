834412281ad0f6e9c7be387bd255d3ffeb320b6a
leo_da_cruz <leonardosilveiracrux@gmail.com>
2024-12-03 03:20:02 -0300

    Estoque testcase att

--- microServicos/src/test/java/prat_eng_soft_api_test_automatizado/TestCase/EstoqueTestCase.java
+++ microServicos/src/test/java/prat_eng_soft_api_test_automatizado/TestCase/EstoqueTestCase.java
@@ -1,11 +1,5 @@
 package prat_eng_soft_api_test_automatizado.TestCase;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,120 +7,204 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
-
-import com.github.javafaker.Faker;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 
 import io.qameta.allure.Allure;
 import io.restassured.response.Response;
-import prat_eng_soft_api_test_automatizado.TestCase.models.EstoqueEntrada;
 import prat_eng_soft_api_test_automatizado.Validation.GenericValidation;
-import prat_eng_soft_api_test_automatizado.utils.ConexaoBancoDados;
+import prat_eng_soft_api_test_automatizado.service.EstoqueService;
 import prat_eng_soft_api_test_automatizado.utils.ContratoManager;
 
-public class EstoqueTestCase extends BaseTestCase {
+@TestMethodOrder(OrderAnnotation.class)
+public class EstoqueTestCase {
 
-    private GenericValidation genericValidation;
-    private ConexaoBancoDados conexaoBancoDados;
+  private GenericValidation genericValidation = new GenericValidation();
+  private EstoqueService estoqueService = new EstoqueService();
 
-    public EstoqueTestCase() {
-        super("estoque", "/v1/stock");
-        genericValidation = new GenericValidation();
-        conexaoBancoDados = new ConexaoBancoDados();
-        // conexaoBancoDados = new ConexaoBancoDados(
-        // "jdbc:mysql://localhost:3306/db_doacao",
-        // "root",
-        // "Leo202426s@");
+  /*
+   * @BeforeAll
+   * public void getToken(){
+   * AuthService authService = new AuthService("baseuri", "rota", "clientid",
+   * "clientsecret");
+   * String Token = authService.getToken();
+   * genericService.addHeader("Authorization", Token);
+   * }
+   */
 
-    }
+  @BeforeEach
+  public void allureReport(TestInfo testInfo) {
+    Allure.epic("Sprint 2");
+    Allure.feature("Micro Serviço de Centro de Estoque");
+  }
 
-    /*
-     * @BeforeAll
-     * public void getToken(){
-     * AuthService authService = new AuthService("baseuri", "rota", "clientid", "clientsecret");
-     * String Token = authService.getToken();
-     * genericService.addHeader("Authorization", Token);
-     * }
-     */
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Incluir nova entrada de Itens no Estoque")
+  @Tag("Regressao")
+  @Order(1)
+  public void incluirNovaEntradaEstoque() {
+    Allure.description("Teste para validar a entrada de um novo item no Estoque");
+    Response resposta = estoqueService.casoFelizEntradaStock();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_CREATED);
+    genericValidation.validarContrato(ContratoManager.getContrato("IncluirNovoEntradaEstoque"));
+  }
 
-    @BeforeEach
-    public void allureReport(TestInfo testInfo) {
-        Allure.epic("Sprint 2");
-        Allure.feature("Micro Serviço de Centro de Estoque");
-    }
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Consultar estoque de um determinado Centro de Distribuição")
+  @Tag("Regressao")
+  @Order(2)
+  public void consultaEstoqueCD() {
+    Allure.description("Teste para validar a consulta de estoque de um determinado Centro de Distribuição");
+    Response resposta = estoqueService.casoFelizConsultarStock();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_OK);
+    genericValidation.validarContrato(ContratoManager.getContrato("consultaEstoqueCD"));
+  }
 
-    @Test
-    @DisplayName("Micro Serviço de Estoque = Incluir nova entrada de Itens no Estoque")
-    @Tag("Regressao")
-    @Order(1)
-    public void incluirNovaEntradaEstoque() {
-        Allure.description("Teste para validar a entrada de um novo item no Estoque");
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Dar baixa de um Itemde um estoque de um determinado Centro de Distribuição")
+  @Tag("Regressao")
+  @Order(3)
+  public void darBaixaEstoque() {
+    Allure.description("Teste para validar a baixa de um item de um Centro de Distribuição");
+    Response resposta = estoqueService.casoFelizDarBaixaEstoque();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_NO_CONTENT);
+  }
 
-        Date data = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String dataFormatada = dateFormat.format(data);
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Saida de um Item de um Centro de Distribuição")
+  @Tag("Regressao")
+  @Order(4)
+  public void consultaQuantidadeItens() {
+    Allure.description("Teste para validar a consulta de quantidade de itens de um Centro de Distribuição");
+    Response resposta = estoqueService.casoFelizConsultarItemDeUmCd();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_OK);
+  }
 
-        Faker faker = new Faker(new Locale("pt-BR"));
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Consultar informando um Centro Distribuição inválido para a pesquisa de estoque de um Centro de Distribuição")
+  @Tag("Regressao")
+  @Order(5)
+  public void consultaEstoqueCDInvalido() {
+    Allure.description("Teste para validar a consulta de estoque de um Centro de Distribuição inválido");
+    Response resposta = estoqueService.casoDeConsultarEstoqueComCDInvalido();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_UNPROCESSABLE_ENTITY);
+    genericValidation.validarContrato(ContratoManager.getContrato("consultarEstoqueErroCd"));
+  }
 
-        EstoqueEntrada estoqueEntradaDTO = new EstoqueEntrada(
-                faker.food().ingredient(),
-                faker.number().numberBetween(1, 1000),
-                faker.food().measurement(),
-                dataFormatada,
-                faker.food().spice());
-        List<EstoqueEntrada> listaEntradas = new ArrayList<>();
-        listaEntradas.add(estoqueEntradaDTO);
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Consultar informando um Centro Distribuição inválido para a pesquisa de um item")
+  @Tag("Regressao")
+  @Order(5)
+  public void consultaItemCentroDistribuicaoInvalido() {
+    Allure.description("Teste para validar a consulta de um item de um Centro de Distribuição inválido");
+    Response resposta = estoqueService.casoDeConsultarItemComCDInvalido();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_UNPROCESSABLE_ENTITY);
+    genericValidation.validarContrato(ContratoManager.getContrato("consultarEstoqueErroCd"));
+  }
 
-        genericService.addPathParam("codCd", 1);
-        genericService.setRota("/{codCd}");
-        genericService.setBody(listaEntradas);
-        // Response resposta = genericService.post("{codCd}", pathParams,
-        // listaEntradas);
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Validar erro ao tentar incluir um item no estoque não informando um nome de item")
+  @Tag("Regressao")
+  @Order(6)
+  public void incluirItemSemNome() {
+    Allure.description("Teste para validar a inclusão de um item no estoque sem informar o nome do item");
+    Response resposta = estoqueService.casoDeIncluirItemSemNome();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_UNPROCESSABLE_ENTITY);
+    genericValidation.validarContrato(ContratoManager.getContrato("IncluirEntradaSemNomeItem"));
+  }
 
-        Response resposta = genericService.post();
-        genericValidation.setResponse(resposta);
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Validar erro ao tentar incluir um item no estoque não informando uma quantidade de item")
+  @Tag("Regressao")
+  @Order(7)
+  public void incluirItemSemQuantidade() {
+    Allure.description("Teste para validar a inclusão de um item no estoque sem informar a quantidade do item");
+    Response resposta = estoqueService.casoDeIncluirItemSemQuantidade();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_UNPROCESSABLE_ENTITY);
+    genericValidation.validarContrato(ContratoManager.getContrato("incluirEntradaSemQuantidadeItem"));
+  }
 
-        genericValidation.validarStatusCode(HttpStatus.SC_CREATED);
-        genericValidation.validarContrato(ContratoManager.getContrato("IncluirNovoEntradaEstoque"));
-        // genericValidation.validarPersistenciaDosDados(true);
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Validar erro ao tentar incluir um item no estoque não informando uma unidade de item")
+  @Tag("Regressao")
+  @Order(8)
+  public void incluirItemSemUnidade() {
+    Allure.description("Teste para validar a inclusão de um item no estoque sem informar a unidade do item");
+    Response resposta = estoqueService.casoDeIncluirItemSemUnidade();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_UNPROCESSABLE_ENTITY);
+    genericValidation.validarContrato(ContratoManager.getContrato("incluirEntradaSemUnidade"));
+  }
 
-    }
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Validar erro ao tentar incluir um item no estoque não informando a categoria do item")
+  @Tag("Regressao")
+  @Order(9)
+  public void incluirItemSemCategoria() {
+    Allure.description("Teste para validar a inclusão de um item no estoque sem informar a categoria do item");
+    Response resposta = estoqueService.casoDeIncluirItemSemCategoria();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_UNPROCESSABLE_ENTITY);
+    genericValidation.validarContrato(ContratoManager.getContrato("incluirEntradaSemCategoria"));
+  }
 
-    @Test
-    @DisplayName("Micro Serviço de Estoque = Consultar estoque de um determinado Centro de Distribuição")
-    @Tag("Regressao")
-    @Order(2)
-    public void consultaEstoqueCD() {
-        Allure.description("Teste para validar a consulta de estoque de um determinado Centro de Distribuição");
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Validar erro ao tentar excluir um item no estoque não informando nome do item")
+  @Tag("Regressao")
+  @Order(10)
+  public void excluirItemSemNome() {
+    Allure.description("Teste para validar a exclusão de um item no estoque sem informar o nome do item");
+    Response resposta = estoqueService.casoDeExcluirItemSemNome();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_UNPROCESSABLE_ENTITY);
+    genericValidation.validarContrato(ContratoManager.getContrato("deletarItemEstoqueSemNome"));
+  }
 
-        conexaoBancoDados.incluirEstoque(14);
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Validar erro ao tentar excluir um item no estoque não informando a quantidade do item")
+  @Tag("Regressao")
+  @Order(11)
+  public void excluirItemSemQuantidade() {
+    Allure.description("Teste para validar a exclusão de um item no estoque sem informar a quantidade do item");
+    Response resposta = estoqueService.casoDeExcluirItemSemQuantidade();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_UNPROCESSABLE_ENTITY);
+    genericValidation.validarContrato(ContratoManager.getContrato("deletarItemEstoqueSemQuantidade"));
+  }
 
-        genericService.addPathParam("codCd", 14);
-        // ;; genericService.addPathParam("codCd", 1);
-        genericService.setRota("/{codCd}");
-        // Response resposta = genericService.get("{codCd}", pathParams);
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Validar erro ao tentar excluir um item no estoque informando uma quantidade maior que a disponível")
+  @Tag("Regressao")
+  @Order(12)
+  public void excluirItemQuantidadeMaiorQueDisponivel() {
+    Allure.description(
+        "Teste para validar a exclusão de um item no estoque informando uma quantidade maior que a disponível");
+    Response resposta = estoqueService.casoDeExcluirItemQuantidadeMaiorQueDisponivel();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_NOT_FOUND);
+    genericValidation.validarMensagem("detail", "Item not found or insufficient quantity");
+    genericValidation.validarContrato(ContratoManager.getContrato("deletarItemNaoEncontrado"));
+  }
 
-        Response resposta = genericService.get();
-        genericValidation.setResponse(resposta);
-        genericValidation.validarStatusCode(HttpStatus.SC_OK);
-        genericValidation.validarContrato(ContratoManager.getContrato("consultaEstoqueCD"));
-    }
-
-    @Test
-    @DisplayName("Micro Serviço de Estoque = Saida de um Item de um Centro de Distribuição")
-    @Tag("Regressao")
-    @Order(4)
-    public void consultaQuantidadeItens() {
-        Allure.description("Teste para validar a consulta de quantidade de itens de um Centro de Distribuição");
-
-        genericService.addPathParam("codCd", 10);
-        genericService.addPathParam("codItem", 10);
-
-        // Response resposta = genericService.get("{codCd}/{codcodBarras}", pathParams);
-
-        Response resposta = genericService.get();
-        genericValidation.setResponse(resposta);
-        genericValidation.validarStatusCode(HttpStatus.SC_OK);
-
-    }
+  @Test
+  @DisplayName("Micro Serviço de Estoque = Validar erro ao tentar excluir um item no estoque informando um item que não existe")
+  @Tag("Regressao")
+  @Order(13)
+  public void excluirItemNaoExistente() {
+    Allure.description("Teste para validar a exclusão de um item no estoque informando um item que não existe");
+    Response resposta = estoqueService.casoDeExcluirItemNaoExistente();
+    genericValidation.setResponse(resposta);
+    genericValidation.validarStatusCode(HttpStatus.SC_NOT_FOUND);
+    genericValidation.validarMensagem("detail", "Item not found or insufficient quantity");
+    genericValidation.validarContrato(ContratoManager.getContrato("deletarItemNaoEncontrado"));
+  }
 
 }
