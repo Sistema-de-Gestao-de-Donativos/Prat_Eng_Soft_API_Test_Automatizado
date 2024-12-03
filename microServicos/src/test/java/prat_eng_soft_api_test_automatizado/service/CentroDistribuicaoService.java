ae0f9a4486059a051a630c77523bb2f8eb7a09a6
leo_da_cruz <leonardosilveiracrux@gmail.com>
2024-12-02 21:15:37 -0300

    CD Service att

---
+++ microServicos/src/test/java/prat_eng_soft_api_test_automatizado/service/CentroDistribuicaoService.java
@@ -0,0 +1,166 @@
+package prat_eng_soft_api_test_automatizado.service;
+
+import java.util.Properties;
+
+import org.apache.http.HttpStatus;
+
+import io.restassured.response.Response;
+import prat_eng_soft_api_test_automatizado.utils.ConexaoBancoDados;
+import prat_eng_soft_api_test_automatizado.utils.models.CentroDistribuicaoSemNumero;
+import prat_eng_soft_api_test_automatizado.utils.models.CentroDistribuicao;
+import prat_eng_soft_api_test_automatizado.utils.models.CentroDistribuicaoAddress;
+
+public class CentroDistribuicaoService extends GenericService {
+    private CentroDistribuicao centroDistribuicao;
+
+    private static String getBaseUri() {
+        Properties propriedades;
+        try {
+            propriedades = ConexaoBancoDados.getProperties("rotas");
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao tentar carregar arquivo com as Rotas", e);
+        }
+        return propriedades.getProperty("cd");
+    }
+
+    private void montarRequisicao() {
+        setBaseUri(getBaseUri());
+        setBasePath("/v1");
+        setRota("/cds");
+    }
+
+    public Response casoFelizListarTodosCentrosDistribuicao() {
+        Response resposta = casoFelizAdcionarCentroDistribuicao();
+        if (resposta.getStatusCode() != HttpStatus.SC_OK) {
+            throw new RuntimeException("Erro ao tentar adicionar um centro de distribuição");
+        }
+        prepararParaNovaRequisicao();
+        montarRequisicao();
+        return get();
+    }
+
+    public Response casoFelizAdcionarCentroDistribuicao(){
+        montarRequisicao();
+        centroDistribuicao = CentroDistribuicao.criarCentroDistribuicao();
+        setBody(centroDistribuicao);
+        return post();
+    }
+
+    public Response casoFelizDeListarCentroDistribuicaoPeloCodigo() {
+        Response resposta = casoFelizAdcionarCentroDistribuicao();
+        if (resposta.getStatusCode() != HttpStatus.SC_OK) {
+            throw new RuntimeException("Erro ao tentar adicionar um abrigo");
+        }
+        prepararParaNovaRequisicao();
+        montarRequisicao();
+        addQueryParams("codCD", resposta.jsonPath().getInt("code"));
+        return get();
+    }
+
+    public Response casoFelizDeListarCentroDistribuicaoPeloNome() {
+        Response resposta = casoFelizAdcionarCentroDistribuicao();
+        if (resposta.getStatusCode() != HttpStatus.SC_OK) {
+            throw new RuntimeException("Erro ao tentar adicionar um abrigo");
+        }
+
+        prepararParaNovaRequisicao();
+        montarRequisicao();
+        addQueryParams("nameCD", centroDistribuicao.getName());
+        return get();
+    }
+
+    public Response casoFelizDeListarCentroDistribuicaoPeloNomeECodigo() {
+        Response resposta = casoFelizAdcionarCentroDistribuicao();
+        if (resposta.getStatusCode() != HttpStatus.SC_OK) {
+            throw new RuntimeException("Erro ao tentar adicionar um abrigo");
+        }
+        prepararParaNovaRequisicao();
+
+        montarRequisicao();
+        addQueryParams("codCD", resposta.jsonPath().getInt("code"));
+        addQueryParams("nameCD", centroDistribuicao.getName());
+        return get();
+    }
+
+    public Response casoErroCriarCentroDistribuicaoSemNome() {
+        montarRequisicao();
+        centroDistribuicao = CentroDistribuicao.criarCentroDistribuicao();
+        centroDistribuicao.setName("");
+        setBody(centroDistribuicao);
+        return post();
+    }
+
+    public Response casoErroCriarCentroDistribuicaoSemTelefone() {
+        montarRequisicao();
+        centroDistribuicao = CentroDistribuicao.criarCentroDistribuicao();
+        centroDistribuicao.setPhone("");
+        setBody(centroDistribuicao);
+        return post();
+    }
+
+    public Response casoErroCriarCentroDistribuicaoSemEmail() {
+        montarRequisicao();
+        centroDistribuicao = CentroDistribuicao.criarCentroDistribuicao();
+        centroDistribuicao.setEmail("");
+        setBody(centroDistribuicao);
+        return post();
+    }
+
+    public Response casoErroCriarCentroDistribuicaoSemPais() {
+        montarRequisicao();
+        centroDistribuicao = CentroDistribuicao.criarCentroDistribuicao();
+        CentroDistribuicaoAddress endereco = centroDistribuicao.getAddress();
+        endereco.setCountry("");
+        centroDistribuicao.setAddress(endereco);
+        setBody(centroDistribuicao);
+        return post();
+    }
+
+    public Response casoErroCriarCentroDistribuicaoSemEstado() {
+        montarRequisicao();
+        centroDistribuicao = CentroDistribuicao.criarCentroDistribuicao();
+        CentroDistribuicaoAddress endereco = centroDistribuicao.getAddress();
+        endereco.setState("");
+        centroDistribuicao.setAddress(endereco);
+        setBody(centroDistribuicao);
+        return post();
+    }
+
+    public Response casoErroCriarCentroDistribuicaoSemCidade() {
+        montarRequisicao();
+        centroDistribuicao = CentroDistribuicao.criarCentroDistribuicao();
+        CentroDistribuicaoAddress endereco = centroDistribuicao.getAddress();
+        endereco.setCity("");
+        centroDistribuicao.setAddress(endereco);
+        setBody(centroDistribuicao);
+        return post();
+    }
+
+    public Response casoErroCriarCentroDistribuicaoSemBairro() {
+        montarRequisicao();
+        centroDistribuicao = CentroDistribuicao.criarCentroDistribuicao();
+        CentroDistribuicaoAddress endereco = centroDistribuicao.getAddress();
+        endereco.setNeighborhood("");
+        centroDistribuicao.setAddress(endereco);
+        setBody(centroDistribuicao);
+        return post();
+    }
+
+    public Response casoErroCriarCentroDistribuicaoSemRua() {
+        montarRequisicao();
+        centroDistribuicao = CentroDistribuicao.criarCentroDistribuicao();
+        CentroDistribuicaoAddress endereco = centroDistribuicao.getAddress();
+        endereco.setStreet("");
+        centroDistribuicao.setAddress(endereco);
+        setBody(centroDistribuicao);
+        return post();
+    }
+
+    public Response casoErroCriarCentroDistribuicaoSemNumero() {
+        montarRequisicao();
+        CentroDistribuicaoSemNumero centroDistribuicaoSemNumero = CentroDistribuicaoSemNumero.criarCentroDistribuicaoSemNumero();
+        setBody(centroDistribuicaoSemNumero);
+        return post();
+    }
+
+}
