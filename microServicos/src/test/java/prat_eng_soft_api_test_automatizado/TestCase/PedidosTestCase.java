daf08673980fa49e14345fb43a4527f9ccc81570
leo_da_cruz <leonardosilveiracrux@gmail.com>
2024-12-03 14:05:48 -0300

    Pedidos testacse att

--- microServicos/src/test/java/prat_eng_soft_api_test_automatizado/TestCase/PedidosTestCase.java
+++ microServicos/src/test/java/prat_eng_soft_api_test_automatizado/TestCase/PedidosTestCase.java
@@ -1,8 +1,5 @@
 package prat_eng_soft_api_test_automatizado.TestCase;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,21 +7,14 @@ import org.junit.jupiter.api.TestInfo;
 
 import io.qameta.allure.Allure;
 import io.restassured.response.Response;
-import prat_eng_soft_api_test_automatizado.TestCase.models.Pedidos;
-import prat_eng_soft_api_test_automatizado.TestCase.models.PedidosItens;
 import prat_eng_soft_api_test_automatizado.Validation.GenericValidation;
-import prat_eng_soft_api_test_automatizado.utils.ConexaoBancoDados;
+import prat_eng_soft_api_test_automatizado.service.PedidosService;
 
-public class PedidosTestCase extends BaseTestCase {
 
-    private GenericValidation genericValidation;
-    private ConexaoBancoDados conexaoBancoDados;
+public class PedidosTestCase  {
 
-    public PedidosTestCase() {
-        super("pedidos", "/v1/pedidos");
-        genericValidation = new GenericValidation();
-        conexaoBancoDados = new ConexaoBancoDados();
-    }
+    private GenericValidation genericValidation= new GenericValidation();
+    private PedidosService pedidosService = new PedidosService();
 
     /*
      * @BeforeAll
@@ -44,27 +34,15 @@ public class PedidosTestCase extends BaseTestCase {
 
     @Test
     public void incluirNovoPedido() {
-
-        PedidosItens pedidosItens = new PedidosItens("12367236d3ba9bb45662982031445", "67236d3ba9bb456629820314", 759);
-        // PedidosItens pedidosItens2 = new PedidosItens("2", "2", 2);
-
-        List<PedidosItens> itens = new ArrayList<>();
-        itens.add(pedidosItens);
-        // itens.add(pedidosItens2);
-
-        Pedidos pedidos = new Pedidos("1", "1", itens);
-
-        genericService.setBody(pedidos);
-        Response response = genericService.post();
-        genericValidation.setResponse(response);
-        genericValidation.validarStatusCode(HttpStatus.SC_CREATED);
-
+      Response response = pedidosService.casoFelizIncluirPedido();
+      genericValidation.setResponse(response);
+      genericValidation.validarStatusCode(HttpStatus.SC_CREATED);
     }
 
     @Test
     public void consultarPedido() {
 
-        Response response = genericService.get();
+        Response response = pedidosService.casoFelizConsultarPedido();
         genericValidation.setResponse(response);
         genericValidation.validarStatusCode(HttpStatus.SC_OK);
 
@@ -72,6 +50,9 @@ public class PedidosTestCase extends BaseTestCase {
 
     @Test
     public void atualizarPedido() {
+    Response response = pedidosService.casoFelizAtualizarPedido();
+      genericValidation.setResponse(response);
+      genericValidation.validarStatusCode(HttpStatus.SC_OK);
 
     }
 
